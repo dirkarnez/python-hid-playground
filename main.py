2d5f2303b3e2e299b28cb138c645852985ad0bfc
dirkarnez <smalldirkalex@gmail.com>
2023-09-27 21:05:45 +0800

    - init

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello world")
+import hid
+
+vid = 1008	# Change it for your device
+pid = 22602	# Change it for your device
+mouse = hid.Device(vid, pid)
+
+while True:
+    try:
+        # Read a raw input report from the mouse
+        raw_data = mouse.read(64, 10000)  # Adjust the buffer size as per your requirement
+        
+        # Process the raw data as needed
+        print(raw_data)
+    except KeyboardInterrupt:
+        break
